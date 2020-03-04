1758b3a46f886e03f7a0cebbabd58dc28b249d7e
netscylla <enquiry@netscylla.com>
2020-03-04 16:27:06 +0000

    started ipv6 support - ripe

--- whereis.py
+++ whereis.py
@@ -26,42 +26,71 @@ def return_ips(iplist):
     cidr_list.append(cidr[0])
   return cidr_list
 
-if len(sys.argv) != 2:
+def return_6ips(iplist):
+  cidr_list=[]
+  #inet6num:       2a00:2381:a66:100::/56
+  cidr_list=re.findall(r'ninet6num:\s+([\w\:\/\d]+)\\n',str(iplist),re.M|re.I)
+  return cidr_list
+
+if len(sys.argv) < 2:
   print("[-] Error: expected a keyword to search for...\n whereis.py [keyword]")
   exit(2)
 
 keyword=sys.argv[1]
 
-# Search ARIN
-cmd="whois -h whois.arin.net 'z / *"+ keyword  +"*'"
-r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
-result,err=r.communicate()
+ipv6 = True if int(sys.argv[2]) == 6 else False
 
-arin_cidrs=return_ips(result)
+if ipv6 == False:
+  # Search ARIN
+  cmd="whois -h whois.arin.net 'z / *"+ keyword  +"*'"
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
 
-#Search RIPE
-cmd="whois -h whois.ripe.net -T inetnum "+ keyword
-r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
-result,err=r.communicate()
+  arin_cidrs=return_ips(result)
 
-ripe_cidrs=return_ips(result)
+  #Search RIPE
+  cmd="whois -h whois.ripe.net -T inetnum "+ keyword
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
 
-#Search AFRINIC
-cmd="whois -h whois.afrinic.net -T inetnum "+ keyword
-r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
-result,err=r.communicate()
+  ripe_cidrs=return_ips(result)
 
-afrinic_cidrs=return_ips(result)
+  #Search AFRINIC
+  cmd="whois -h whois.afrinic.net -T inetnum "+ keyword
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
 
-#Search APNIC
-cmd="whois -h whois.apnic.net -T inetnum "+ keyword
-r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
-result,err=r.communicate()
+  afrinic_cidrs=return_ips(result)
 
-apnic_cidrs=return_ips(result)
+  #Search APNIC
+  cmd="whois -h whois.apnic.net -T inetnum "+ keyword
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
 
-#add all cidrs together & uniq
-cidr_list=arin_cidrs+ripe_cidrs+afrinic_cidrs+apnic_cidrs
-f_list=unique(cidr_list)
-for cidr in f_list:
-  print(cidr)
+  apnic_cidrs=return_ips(result)
+
+  #add all cidrs together & uniq
+  cidr_list=arin_cidrs+ripe_cidrs+afrinic_cidrs+apnic_cidrs
+  f_list=unique(cidr_list)
+  for cidr in f_list:
+    print(cidr)
+else:
+  #Search ARIN
+  cmd="whois -h whois.arin.net 'z / *"+ keyword  +"*'"
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
+  arin_cidrs=return_6ips(result)
+
+  #Search RIPE
+  cmd="whois -h whois.ripe.net -T inet6num "+ keyword
+  r = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=False)
+  result,err=r.communicate()
+  ripe_cidrs=return_6ips(result)
+
+  #add all cidrs together & uniq
+  #cidr_list=arin_cidrs+
+  cidr_list=ripe_cidrs
+  #+afrinic_cidrs+apnic_cidrs
+  f_list=unique(cidr_list)
+  for cidr in f_list:
+    print(cidr)
